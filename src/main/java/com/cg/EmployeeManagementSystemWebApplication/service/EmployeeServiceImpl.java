5ab5f5f5e37adeabbb59fd66f0a824bd08231462
chandana gone <gonechandana>
2025-07-12 12:21:30 +0530

    server

--- src/main/java/com/cg/EmployeeManagementSystemWebApplication/service/EmployeeServiceImpl.java
+++ src/main/java/com/cg/EmployeeManagementSystemWebApplication/service/EmployeeServiceImpl.java
@@ -3,37 +3,34 @@ package com.cg.EmployeeManagementSystemWebApplication.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service; // ✅ Make sure to import this
 
 import com.cg.EmployeeManagementSystemWebApplication.entity.Employee;
 import com.cg.EmployeeManagementSystemWebApplication.repository.EmployeeRepo;
 
+@Service  // ✅ This tells Spring to treat this as a bean
 public class EmployeeServiceImpl implements EmployeeService {
-	@Autowired
- EmployeeRepo repo;
-	@Override
-	public List<Employee> getAllEmployees() {
-		// TODO Auto-generated method stub
-		return repo.findAll();
-	}
 
-	@Override
-	public void saveEmployee(Employee employee) {
-		// TODO Auto-generated method stub
-		repo.save(employee);
-	}
+    @Autowired
+    private EmployeeRepo repo;
 
-	@Override
-	public Employee getEmployeeById(int id) {
-		// TODO Auto-generated method stub
-	   return repo.findById(id).get();
-	}
+    @Override
+    public List<Employee> getAllEmployees() {
+        return repo.findAll();
+    }
 
-	@Override
-	public void deleteById(int id) {
-		// TODO Auto-generated method stub
-		repo.deleteById(id);
-	}
-	
-	
+    @Override
+    public void saveEmployee(Employee employee) {
+        repo.save(employee);
+    }
 
+    @Override
+    public Employee getEmployeeById(int id) {
+        return repo.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
 }
